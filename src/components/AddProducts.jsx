eda9ad7e1e61b6a3edddf5177441470a90313b4b
galaxemirata <collinsdmwas@gmail.com>
2026-03-17 13:35:59 +0300

    This contains the addproducts and getproducts apis

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const AddProducts = () => {
+
+  const[product_name,setProduct_name]=useState()
+  const[product_description,setProduct_description]=useState()
+  const[product_cost,setProduct_cost]=useState()
+  const[product_photo,setProduct_photo]=useState()
+
+  const[loading,setLoading]=useState()
+  const[success,setSuccess]=useState()
+  const[error,setError]=useState()
+
+  const submit=async(e)=>{
+    e.preventDefault()
+    setLoading("Kindly wait as we upload your Product")
+
+    try {
+      const data=new FormData()
+        data.append("product_name",product_name)
+        data.append("product_description",product_description)
+        data.append("product_cost",product_cost)
+        data.append("product_photo",product_photo)
+
+
+        const response=await axios.post("http://collins.alwaysdata.net/api/addproducts",data)
+
+      setLoading("")
+      setSuccess(response.data.message)
+
+      setProduct_name("")
+      setProduct_description("")
+      setProduct_cost("")
+      setProduct_photo("")
+
+      }
+
+      
+      
+
+
+      
+     catch (error) {
+      setLoading("")
+      setError(error.message)
+      
+      
+    }
+
+
+
+
+
+
+
+  }
+
+  
+
   return (
-    <div>
-        <h3>Welcome to add products</h3>
+    <div className='row justify-content-center'>
+      <div className='col-md-6 card shadow'>
+        <h1>Upload Products</h1>
+        <form action="" onSubmit={submit}>
+          <p className='text-warning'>{loading}</p>
+          <p className='text-success'>{success}</p>
+          <p className='text-danger'>{error}</p>
+
+          <input type="text" placeholder='Enter Product Name ' className='form-control' value={product_name} onChange={(e)=>setProduct_name(e.target.value)}/>
+          <br />
+          <br />
+          <textarea name="" id="" placeholder='Describe your product ' className='form-control' value={product_description}onChange={(e)=>setProduct_description(e.target.value)}></textarea>
+          <br />
+          <br />
+          <input type="text" placeholder='Enter product Cost'className='form-control' value={product_cost} onChange={(e)=>setProduct_cost(e.target.value)}/>
+          <br />
+          <br />
+          <b>Upload product photo</b>
+          <br />
+          <br />
+          <input type="file" className='form-control' accept='image/*' onChange={(e)=>setProduct_photo(e.target.files[0])}/>
+          <br />
+          <input type="submit" value={"Upload product"} className='text-white bg-primary form-control'/>
+          <br />
+
+        </form>
+
+
+      </div>
+        
     </div>
   )
 }
--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const GetProducts = () => {
+
+  const[loading,setLoading]=useState("")
+  const[error,setError]=useState("")
+  const[products,setproducts]=useState([])
+
+  // variable to store an image
+  const image_url="http://collins.alwaysdata.net/static/images/"
+
+  // create a function to get our products from the api
+
+  const fetchproducts=async()=>{
+
+    setLoading("Please wait as we retrieve your products")
+
+
+    try {
+      
+      const response= await axios.get("http://collins.alwaysdata.net/api/getproductdetails")
+      console.log("the response is",response)
+      setproducts(response.data)
+      setLoading("")
+
+
+
+    } catch (error) {
+      setLoading("")
+      setError(error.message)
+      
+    }
+
+
+
+
+
+
+  }
+
+  // end of function to call the useEffect
+
+  useEffect(()=>{
+    fetchproducts()
+  },[])
+
   return (
-    <div>
-        <h3>Welcome to Get Products</h3>
+    <div className='row'>
+      <h1>Available Products</h1>
+
+      <p className='text-warning'>{loading}</p>
+      <p className='text-danger'>{error}</p>
+
+      {/* loop through our products and bring one by one */}
+      {products.map((product)=>(
+
+      
+      <div className='col-md-3 justify-content-center'>
+
+        <div className='card shadow'>
+          
+        <img src={image_url+product.product_photo} alt="cake" className='product_img'/>
+        
+        <div className='card-body'> 
+          <h4 className='text-success'>{product.product_name}</h4>
+          <p className='text-secondary'>{product.product_description}</p>
+          <p className='text-warning'>{product.product_cost}</p>
+          <input type="btn" className='btn bg-secondary text-white' value={"Make payment"} />
+          
+          
+
+
+        </div>
+        </div>
+
+        
+      </div>
+      ))}
+      
+        
     </div>
   )
 }
